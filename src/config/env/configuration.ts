6cd3d11d6447c3b49b1f006230981ae41dfc23bd
Yoshitaka195 <mznm0824.h.n@gmail.com>
2025-10-30 09:30:36 +0900

    fix: 環境変数名

--- src/config/env/configuration.ts
+++ src/config/env/configuration.ts
@@ -1,9 +1,9 @@
 export default () => ({
   main_database: {
-    host: process.env.RAKBIL_TEST_DB_HOST,
-    username: process.env.RAKBIL_TEST_DB_USER,
-    port: process.env.RAKBIL_TEST_DB_PORT,
-    password: process.env.RAKBIL_TEST_DB_PASSWORD,
-    name: process.env.RAKBIL_TEST_DB,
+    host: process.env.TEST_DB_HOST,
+    username: process.env.TEST_DB_USER,
+    port: process.env.TEST_DB_PORT,
+    password: process.env.TEST_DB_PASSWORD,
+    name: process.env.TEST_DB,
   },
 });
